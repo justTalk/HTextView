650981351fb534a8528ab37c079f649037dfc992
mingming.liu@quvideo.com <946832374l@gmail.com>
2025-05-08 13:33:52 +0800

    升级到andoridx

--- demoapp/src/main/java/com/example/demoapp/BaseActivity.java
+++ demoapp/src/main/java/com/example/demoapp/BaseActivity.java
@@ -1,10 +1,10 @@
 package com.example.demoapp;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import com.hanks.htextview.base.AnimationListener;
 import com.hanks.htextview.base.HTextView;
 
--- demoapp/src/main/java/com/example/demoapp/MainActivity.java
+++ demoapp/src/main/java/com/example/demoapp/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.demoapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * MainActivity
